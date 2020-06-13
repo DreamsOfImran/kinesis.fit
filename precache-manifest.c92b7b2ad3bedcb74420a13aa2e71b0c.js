9130fecf93e9e99ae55951c283c598c930d075d2
DreamsOfImran <ibasha66@gmail.com>
2020-06-13 10:22:04 +0530

    Updates

---
+++ precache-manifest.c92b7b2ad3bedcb74420a13aa2e71b0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9763620e0ee4c2ae12b8fe40261c4d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4352a38cab87389bef69",
+    "url": "/static/css/main.7ff7ce7e.chunk.css"
+  },
+  {
+    "revision": "17fa8115cec368b007fa",
+    "url": "/static/js/2.7eac1fb4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7eac1fb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4352a38cab87389bef69",
+    "url": "/static/js/main.e7d8be58.chunk.js"
+  },
+  {
+    "revision": "e785149728c10cdc90a4",
+    "url": "/static/js/runtime-main.3bf6d26c.js"
+  }
+]);
